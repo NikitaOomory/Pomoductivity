c6f16c555e15fe1c5616d91adb967803f48f3fc4
Никита Васильев <n.vasilev@corp.mkskom.com>
2024-05-30 19:05:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pomoductivity/adapter/NoteAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.pomoductivity.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesroompractice.databinding.NoteLayoutBinding
+import com.example.pomoductivity.fragments.HomeFragmentDirections
+import com.example.pomoductivity.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val itemBinding: NoteLayoutBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Note>(){
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.noteDesk == newItem.noteDesk &&
+                    oldItem. noteTitle == newItem.noteTitle
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    var differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemBinding.noteTitle.text = currentNote.noteTitle
+        holder.itemBinding.noteDesc.text = currentNote.noteDesk
+
+        holder.itemView.setOnClickListener{
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNote)
+            it.findNavController().navigate(direction)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/pomoductivity/fragments/EditNoteFragment.kt
@@ -0,0 +1,103 @@
+package com.example.pomoductivity.fragments
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.notesroompractice.R
+import com.example.notesroompractice.databinding.FragmentEditNoteBinding
+import com.example.pomoductivity.MainActivity
+import com.example.pomoductivity.model.Note
+import com.example.pomoductivity.viewmodel.NoteViewModel
+
+
+class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
+
+    private var editNoteBinding: FragmentEditNoteBinding? = null
+    private val binding get() = editNoteBinding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var currentNote: Note
+
+    private val args: EditNoteFragmentArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        editNoteBinding = FragmentEditNoteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        notesViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note!!
+
+        binding.editNoteTitle.setText(currentNote.noteTitle)
+        binding.editNoteDesc.setText(currentNote.noteDesk)
+
+        binding.editNoteFab.setOnClickListener{
+           val noteTitle = binding.editNoteTitle.text.toString().trim()
+            val noteDesc = binding.editNoteDesc.text.toString().trim()
+
+            if(noteTitle.isNotEmpty()){
+                val note = Note(currentNote.id, noteTitle, noteDesc)
+                notesViewModel.updateNote(note)
+                view.findNavController().popBackStack(R.id.homeFragment, false)
+            }else{
+                Toast.makeText(context, "Заполните заголовок", Toast.LENGTH_SHORT)
+            }
+        }
+    }
+
+    private fun deleteNote(){
+        AlertDialog.Builder(activity).apply {
+            setTitle("Удаление заметки")
+            setMessage("Вы хотите удалить заметку?")
+            setPositiveButton("да"){_,_ ->
+                notesViewModel.deleteNote(currentNote)
+                Toast.makeText(context,"Заметка удалена", Toast.LENGTH_SHORT)
+                view?.findNavController()?.popBackStack(R.id.homeFragment, false)
+            }
+            setNegativeButton("Нет", null)
+        }.create().show()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId){
+            R.id.deleteMenu -> {
+                deleteNote()
+                true
+            }else -> false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        editNoteBinding = null
+    }
+
+}
